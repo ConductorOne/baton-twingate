abedc8d9afe71e4f4f7b690aa72aed9c7c64fa3a
Javier David Carnelli <javier.carnelli@conductorone.com>
2025-07-14 12:06:35 -0300

    chore: addressing PR comments

--- cmd/baton-twingate/config.go
+++ cmd/baton-twingate/config.go
@@ -7,14 +7,18 @@ import (
 var (
 	DomainField = field.StringField(
 		"domain",
+		field.WithDisplayName("Domain"),
 		field.WithDescription("The domain for your Twingate account. ($BATON_DOMAIN)"),
 		field.WithRequired(true),
 	)
 	ApiKeyField = field.StringField(
 		"api-key",
+		field.WithDisplayName("API Key"),
 		field.WithDescription("The api key for your Twingate account. ($BATON_API_KEY)"),
 		field.WithRequired(true),
+		field.WithIsSecret(true),
 	)
+
 	configurationFields = []field.SchemaField{
 		DomainField,
 		ApiKeyField,
